89fd184a2ee8fef354a8052aef4a543f90f58786
Pooh555 <peerasit.lu@gmail.com>
2025-02-07 00:21:53 +0700

    Add carousel slider

--- lib/Themes/path.dart
+++ lib/Themes/path.dart
@@ -1,7 +1,7 @@
-List<String> home_carousel_images = [
-  "res/assets/images/home_carousel/carousel_image.1.jpg",
-  "res/assets/images/home_carousel/carousel_image.2.jpg",
-  "res/assets/images/home_carousel/carousel_image.3.jpg",
-  "res/assets/images/home_carousel/carousel_image.4.jpg",
-  "res/assets/images/home_carousel/carousel_image.5.jpg",
+List<String> home_carousel_images_path = [
+  "assets/images/home_carousel/carousel_image.1.jpg",
+  "assets/images/home_carousel/carousel_image.2.jpg",
+  "assets/images/home_carousel/carousel_image.3.jpg",
+  "assets/images/home_carousel/carousel_image.4.jpg",
+  "assets/images/home_carousel/carousel_image.5.jpg",
 ];
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import '../Themes/themes.dart';
-import '../Themes/path.dart';
+import 'package:ai_generated_content_detector/home/carousel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -14,49 +13,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(0.0),
-        child: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-      ),
-      body: const CarouselPanel(),
-    );
-  }
-}
-
-class CarouselPanel extends StatefulWidget {
-  const CarouselPanel({super.key});
-
-  @override
-  State<CarouselPanel> createState() => _CarouselPanelState();
-}
-
-class _CarouselPanelState extends State<CarouselPanel> {
-  final CarouselController controller = CarouselController(initialItem: 1);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxHeight: 200),
-      child: CarouselView(
-        itemExtent: 330,
-        shrinkExtent: 200,
-        padding: const EdgeInsets.all(10.0),
-        children: List.generate(
-          home_carousel_images.length,
-          (index) => Image.asset(
-            home_carousel_images[index],
-            fit: BoxFit.cover,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0.0),
+          child: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            const CarouselPanel(),
+          ],
+        ));
   }
 }
