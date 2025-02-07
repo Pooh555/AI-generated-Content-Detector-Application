b240f90b0e381ac5f0a979c3a709ac12158be496
Pooh555 <peerasit.lu@gmail.com>
2025-02-08 01:14:00 +0700

    Create a rough about page

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -1,5 +1,7 @@
 import 'package:ai_generated_content_detector/themes/template.dart';
+import 'package:ai_generated_content_detector/themes/varaibles.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatefulWidget {
   const About({super.key, required this.title});
@@ -11,9 +13,145 @@ class About extends StatefulWidget {
 
 class _AboutState extends State<About> {
   @override
-  @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
     return Scaffold(
-        appBar: MyAppbar(title: "About this application"), body: Container());
+      appBar: MyAppbar(title: "About this application"),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(screenBorderMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: "AI vs Human", style: textTheme.headlineMedium),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("res/assets/github/kita_AI.jpg",
+                    width: MediaQuery.of(context).size.width * 0.45),
+                Image.asset("res/assets/github/kita_human.jpg",
+                    width: MediaQuery.of(context).size.width * 0.45),
+              ],
+            ),
+            const SizedBox(height: 15),
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: "About This Project",
+                      style: textTheme.headlineMedium),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            InkWell(
+              onTap: () async {
+                final Uri url = Uri.parse(
+                    "https://www.kaggle.com/competitions/detect-ai-vs-human-generated-images");
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text:
+                          "This project is an extension a Kaggle competition.",
+                      style: textTheme.bodySmall
+                          ?.copyWith(color: colorScheme.surfaceTint),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: "Deep Learning", style: textTheme.headlineMedium),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            RichText(
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text:
+                          "The models used for identifying AI-generated content are listed below.",
+                      style: textTheme.bodySmall),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            InkWell(
+              onTap: () async {
+                final Uri url = Uri.parse(
+                    "https://github.com/Pooh555/AI-vs-human-generated-image");
+                if (await canLaunchUrl(url)) {
+                  await launchUrl(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "  - Image model (link)",
+                      style: textTheme.bodySmall
+                          ?.copyWith(color: colorScheme.surfaceTint),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: "We are from Kamneotvidya Science Academy.",
+                        style: textTheme.bodySmall),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: InkWell(
+                onTap: () async {
+                  final Uri url = Uri.parse("https://www.kvis.ac.th");
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                child: const Text(
+                  "KVIS",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
